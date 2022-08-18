065843ae4e71912455a427a2ef0433c9f3234077
Anatolii <zavorinanatolii@mail.ru>
2022-08-18 22:05:53 +0800

    Dz-29

--- Dz-25/Program.cs
+++ Dz-25/Program.cs
@@ -5,15 +5,18 @@ Console.WriteLine("Введите число A: ");
 int A = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите степень B: ");
 int B = int.Parse(Console.ReadLine());
+/* Console.WriteLine("Введите степень C: ");  // работа с дополнительной степенью "C"
+int C = int.Parse(Console.ReadLine()); */
 
-int Nums(int B)
+int Nums(int num)  // num -  название переменной, с которой работаем в цикле
 { 
     int pro = 1;
-    for (int i = 1; i <= B; i++) // без цикла можно воспользоваться Math.Pow(A, B);
+    for (int i = 1; i <= num; i++) // без цикла можно воспользоваться Math.Pow(A, B);
     {
         pro = pro * A;  
         //Console.WriteLine(pro); // для наглядности
     }
     return(pro);
 }
-Console.WriteLine(Nums(B));
+Console.WriteLine(Nums(B)); //  B - введенное число, с которым работает функция, вместо "int num" 
+// Console.WriteLine(Nums(C)); // - работа с дополнительной степенью "C", не нужно писать отдельный цикл для каждго решения.
---
+++ Dz-29/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами 
+из [a, b) и выводит их на экран. */
+
+
+Console.WriteLine("Введите количество элементов массива: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+
+/*  if (A >B)
+{
+    int min = B;
+    int max = A;
+}
+else
+{
+    int min = A;
+    int max = B;
+}
+
+Console.WriteLine(min, max); */
+
+void Rand()
+{
+    int[] arriva = new int [N];
+
+    for (int i = 0; i < arriva.Length; i++)  /* arr - название массива, Length - длина массива, 
+    всегда вернет длину массива, выгоднее, чем использование конкретной цифры, при изменении длины массива, 
+    чтобы не забыть и не запутаться Например, выше длину массива 8 можно поменять на любую*/
+    {
+        arriva[i] = new Random().Next(A,B);
+        //Console.Write("{0} ", arriva[i]); первый метод вывода
+    }
+    Console.WriteLine($"[{String.Join(",", arriva)}]"); // Второй метод вывода, String - строка
+}
+
+Rand();
