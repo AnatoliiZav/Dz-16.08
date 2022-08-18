ea8a36c33b049b2f73aef3d836295f6dba69107b
Anatolii <zavorinanatolii@mail.ru>
2022-08-18 22:14:30 +0800

    Dz-29

--- Dz-29/Program.cs
+++ Dz-29/Program.cs
@@ -9,31 +9,15 @@ int A = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int B = int.Parse(Console.ReadLine());
 
-/*  if (A >B)
-{
-    int min = B;
-    int max = A;
-}
-else
-{
-    int min = A;
-    int max = B;
-}
-
-Console.WriteLine(min, max); */
-
 void Rand()
 {
     int[] arriva = new int [N];
 
-    for (int i = 0; i < arriva.Length; i++)  /* arr - название массива, Length - длина массива, 
-    всегда вернет длину массива, выгоднее, чем использование конкретной цифры, при изменении длины массива, 
-    чтобы не забыть и не запутаться Например, выше длину массива 8 можно поменять на любую*/
+    for (int i = 0; i < arriva.Length; i++)  
     {
         arriva[i] = new Random().Next(A,B);
-        //Console.Write("{0} ", arriva[i]); первый метод вывода
     }
-    Console.WriteLine($"[{String.Join(",", arriva)}]"); // Второй метод вывода, String - строка
+    Console.WriteLine($"[{String.Join(",", arriva)}]"); 
 }
 
 Rand();
