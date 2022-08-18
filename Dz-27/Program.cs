6bc61af98c7c6240fdd8b0402426666f53d061f4
Anatolii <zavorinanatolii@mail.ru>
2022-08-18 17:00:05 +0800

    Dz-27

---
+++ Dz-27/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите функцию и запустите ее, которая принимает на вход
+число  и выдает сумму цифр в числе. */
+
+Console.WriteLine("Введите число: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumNums(X)); 
+
+int SumNums(int nu)
+{
+    int sum = 0;
+    while ( nu > 0 )
+    {
+        int b = nu % 10;
+        sum = sum + b;  
+        nu = nu / 10;
+    }
+    return(sum);  
+}
+
